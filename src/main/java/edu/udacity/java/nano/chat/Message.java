6a8dd5a3400ac02322cd975ac56ea17d5296f884
Guney <info@guney.ch>
2019-09-13 14:43:18 +0200

    chatserver commit test

---
+++ src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -0,0 +1,80 @@
+package edu.udacity.java.nano.chat;
+
+import com.google.gson.Gson;
+
+import javax.websocket.EncodeException;
+
+
+/**
+ * WebSocket message model
+ */
+public class Message {
+    private String type;
+    private String content;
+    private String sender;
+    private int onlineUserCount;
+
+    private static Gson gson = new Gson();
+
+
+    public Message(String message) {
+    }
+
+    public Message(String type, String content, String sender, int onlineUserCount) {
+        this.type = type;
+        this.content = content;
+        this.sender = sender;
+        this.onlineUserCount = onlineUserCount;
+    }
+
+    public Message() {
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public int getOnlineCount() {
+        return onlineUserCount;
+    }
+
+    public void setOnlineCount(int onlineCount) {
+        this.onlineUserCount = onlineCount;
+    }
+
+    public String encode(Message message) throws EncodeException {
+        return gson.toJson(message);
+    }
+    /*
+    public String toJsonStr() {
+        return "{"
+                + "\"Content\": \"" + this.content + "\", "
+                + "\"sender\": \"" + this.sender + "\", "
+                + "\"type\": \"" + this.type + "\", "
+                + "\"onlineUserCount\": \"" + this.onlineUserCount
+                + "\" }";
+    }
+
+     */
+}
---
+++ src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -0,0 +1,114 @@
+package edu.udacity.java.nano.chat;
+
+import edu.udacity.java.nano.chat.Message;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * WebSocket Server
+ *
+ * @see ServerEndpoint WebSocket Client
+ * @see Session   WebSocket Session
+ */
+
+@Component
+@ServerEndpoint("/chat/{username}")
+public class WebSocketChatServer {
+
+    /**
+     * All chat sessions.
+     */
+
+    private Session session;
+    private static Set<WebSocketChatServer> chatEndpoints
+            = new CopyOnWriteArraySet<WebSocketChatServer>();
+
+
+    private static HashMap<String, String> users = new HashMap<>();
+    private static Map<String, Session>  sessionMap = new ConcurrentHashMap<>();
+
+
+
+    private static void sendMessageToAll(Session session, Message message) throws IOException, EncodeException {
+        message.setSender(users.get(session.getId()));
+        broadcast(message);
+    }
+    private static void sendMessageToAll(Message message) throws IOException, EncodeException {
+        message.setSender(message.getSender());
+        broadcast(message);
+    }
+
+    /**
+     * Open connection, 1) add session, 2) add user.
+     */
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) throws IOException, EncodeException {
+        this.session = session;
+        chatEndpoints.add(this);
+        users.put(session.getId(), username );
+
+        sessionMap.put(session.getId(), session);
+
+        Message message = new Message();
+        message.setSender(username);
+        message.setContent("Connected!");
+        message.setContent("Disconnected!");
+
+        broadcast(message);
+    }
+
+    /**
+     * Send message, 1) get username and session, 2) send message to all.
+     */
+    @OnMessage
+    public void onMessage(Session session,  String  message) throws IOException, EncodeException {
+        Message messages = new Message(message);
+       sendMessageToAll(messages);
+    }
+
+    /**
+     * Close connection, 1) remove session, 2) update user.
+     */
+    @OnClose
+    public void onClose(Session session) throws IOException, EncodeException {
+        //TODO: add close connection.
+        chatEndpoints.remove(this);
+        sessionMap.remove(session.getId());
+        Message message = new Message();
+        message.setSender(users.get(session.getId()));
+        message.setContent("Disconnected!");
+        broadcast(message);
+    }
+
+    /**
+     * Print exception.
+     */
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+    private static void broadcast(Message message)
+            throws IOException, EncodeException {
+
+        chatEndpoints.forEach(endpoint -> {
+            synchronized (endpoint) {
+                try {
+                    endpoint.session.getBasicRemote().
+                            sendObject(message);
+                } catch (EncodeException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
